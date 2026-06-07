1604880560cb1ff7c27b67a5bd509e3ad20d1ce3
ostymate <ostymate@gmail.com>
2026-06-08 01:16:02 +0500

    Lazy init added

--- platforms/bb_avr.c
+++ platforms/bb_avr.c
@@ -8,6 +8,7 @@
 #include "bb_avr.h"
 
 volatile uint32_t _bb_timer1_overflow = 0;
+static bool uint32_t _bb_timer1_init = false;
 
 ISR(TIMER1_OVF_vect)
 {
@@ -16,11 +17,14 @@ ISR(TIMER1_OVF_vect)
 
 void BB_TICKS_SOURCE_INIT(void)
 {
+    if (_bb_timer1_init) 
+        return;
     _bb_timer1_overflow = 0;
     TCCR1A = 0;
     TCCR1B = (1 << CS10);
     TCNT1 = 0;
     TIMSK1 |= (1 << TOIE1);
+    _bb_timer1_init = true;
 }
 
 #endif /* AVR */
