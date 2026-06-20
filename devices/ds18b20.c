f7c3b9d7978504b5b30e81bd7f67220c4f42b586
ostymate <ostymate@gmail.com>
2026-06-20 23:32:06 +0500

    refactor ds18b20:  ds18b20_update() returns temperature "age" in microseconds

--- devices/ds18b20.c
+++ devices/ds18b20.c
@@ -1,93 +1,91 @@
 #include "ds18b20.h"
 
-// 1-Wire commands
+/* 1-Wire commands */
 #define DS18B20_CMD_CONVERT_TEMP 0x44
 #define DS18B20_CMD_READ_SCRATCHPAD 0xBE
 
-// valid values
+/* temperature valid limits */
 #define DS18B20_MIN_VALID_TEMP_X10 -550 // -55.0
 #define DS18B20_MAX_VALID_TEMP_X10 1250 // 125.0
-#define DS18B20_INIT_VALUE 850
 
-#define DS18B20_CONVERSION_US 750000 // 750ms conversion time (12-bit)
+#define SCRATCHPAD_SIZE 9
+#define CONVERSION_NEVER_STARTED UINT32_MAX
 
-void ds18b20_init(ds18b20_sensor_t *sensor, gpio_pin_t data_pin)
+/* 750ms conversion interval (12-bit) */
+#define DS18B20_CONVERSION_US 750000
+
+static bool start_conversion(ds18b20_sensor_t *sensor)
+{
+    if (!onewire_check_presence(sensor->data_pin))
+        return false;
+    onewire_write_byte(sensor->data_pin, ONEWIRE_CMD_SKIP_ROM);
+    onewire_write_byte(sensor->data_pin, DS18B20_CMD_CONVERT_TEMP);
+    GET_TICK(sensor->conversion_started);
+    return true;
+}
+
+static int16_t read_scratchpad(ds18b20_sensor_t *sensor)
+{
+
+    if (!onewire_check_presence(sensor->data_pin))
+        return DS18B20_INIT_VALUE;
+
+    onewire_write_byte(sensor->data_pin, ONEWIRE_CMD_SKIP_ROM);
+    onewire_write_byte(sensor->data_pin, DS18B20_CMD_READ_SCRATCHPAD);
+
+    uint8_t scratchpad[SCRATCHPAD_SIZE];
+    for (int i = 0; i < SCRATCHPAD_SIZE; i++)
+        scratchpad[i] = onewire_read_byte(sensor->data_pin);
+
+    if (!(onewire_crc8(scratchpad, 8) == scratchpad[8]))
+        return DS18B20_INIT_VALUE;
+
+    int16_t temp_x10 = ((scratchpad[1] << 8) | scratchpad[0]) * 10 / 16;
+
+    if (temp_x10 < DS18B20_MIN_VALID_TEMP_X10 || temp_x10 > DS18B20_MAX_VALID_TEMP_X10)
+        return DS18B20_INIT_VALUE;
+
+    return temp_x10;
+}
+
+bool ds18b20_init(ds18b20_sensor_t *sensor, gpio_pin_t data_pin)
 {
 
     sensor->data_pin = data_pin;
-    sensor->is_active = false;
-    sensor->is_converting = false;
-    sensor->error_count = 0;
-    sensor->success_count = 0;
+    sensor->temp_x10 = DS18B20_INIT_VALUE;
+    sensor->last_valid_conversion = DS18B20_TEMP_NEVER_UPDATED;
+    sensor->conversion_started = CONVERSION_NEVER_STARTED;
     TICK_INIT();
+    return start_conversion(sensor);
 }
 
-static void ds18b20_process_error(ds18b20_sensor_t *sensor)
-{
-    sensor->error_count++;
-    sensor->is_active = false;
-    sensor->is_converting = false;
-}
-
-void ds18b20_update(ds18b20_sensor_t *sensor)
+uint32_t ds18b20_update(ds18b20_sensor_t *sensor, float *temp)
 {
     if (!sensor)
-        return;
+        return DS18B20_TEMP_NEVER_UPDATED;
 
     uint32_t now;
     GET_TICK(now);
 
-    if (sensor->is_converting)
+    if (now - sensor->conversion_started > US_TO_TICKS(DS18B20_CONVERSION_US))
     {
-        if ((now - sensor->conversion_started) < US_TO_TICKS(DS18B20_CONVERSION_US))
-            return;
 
-        
-        //  Read temperature data
-        if (onewire_check_presence(sensor->data_pin))
+        int16_t temp_x10 = read_scratchpad(sensor);
+        start_conversion(sensor);
+
+        if (temp_x10 != DS18B20_INIT_VALUE)
         {
-            onewire_write_byte(sensor->data_pin, ONEWIRE_CMD_SKIP_ROM);
-            onewire_write_byte(sensor->data_pin, DS18B20_CMD_READ_SCRATCHPAD);
-
-            for (int i = 0; i < 9; i++)
-            {
-                sensor->scratchpad[i] = onewire_read_byte(sensor->data_pin);
-            }
-
-            // Validate and process data
-            if (onewire_crc8(sensor->scratchpad, 8) == sensor->scratchpad[8])
-            {
-                int16_t temp = ((sensor->scratchpad[1] << 8) | sensor->scratchpad[0]) * 10 / 16;
-
-                if (temp >= DS18B20_MIN_VALID_TEMP_X10 && temp <= DS18B20_MAX_VALID_TEMP_X10 && temp != DS18B20_INIT_VALUE)
-                {
-                    sensor->temperature_x10 = temp;
-                    sensor->success_count++;
-                    sensor->is_active = true;
-                    sensor->is_converting = false;
-                }
-                else
-                    ds18b20_process_error(sensor);
-            }
-            else
-                ds18b20_process_error(sensor);
+            sensor->temp_x10 = temp_x10;
+            GET_TICK(sensor->last_valid_conversion);
+            GET_TICK(now);
         }
-        else
-            ds18b20_process_error(sensor);
-       
     }
-    else
-    {
-        
-        if (onewire_check_presence(sensor->data_pin))
-        {
-            onewire_write_byte(sensor->data_pin, ONEWIRE_CMD_SKIP_ROM);
-            onewire_write_byte(sensor->data_pin, DS18B20_CMD_CONVERT_TEMP);
-            GET_TICK(sensor->conversion_started);
-            sensor->is_converting = true;
-        }
-        else
-            ds18b20_process_error(sensor);
-       
-    }
+
+    if (temp)
+        *temp = (float)sensor->temp_x10 * 0.1f;
+
+    if (sensor->last_valid_conversion == DS18B20_TEMP_NEVER_UPDATED && sensor->conversion_started == CONVERSION_NEVER_STARTED)
+        return DS18B20_TEMP_NEVER_UPDATED;
+
+    return (now - sensor->last_valid_conversion) / US_TO_TICKS(1);
 }
--- devices/ds18b20.h
+++ devices/ds18b20.h
@@ -1,6 +1,6 @@
 /**
  * @file ds18b20.h
- * @brief ds18b20 driver based on simple software bitbang 
+ * @brief non-blocking ds18b20 driver based on simple software bitbang 
  */
 
 #pragma once
@@ -11,42 +11,44 @@ extern "C" {
 
 #include "../core/onewire.h"
 
+#define DS18B20_INIT_VALUE 850
+#define DS18B20_TEMP_NEVER_UPDATED UINT32_MAX
+
 /**
- * @brief ds18b20 sensor 
- * @param data_pin onewire bus data pin 
- * @param temperature_x10 last measured temperature in fixed point format e.g. 225 means 22.5°C
- * @param error_count number of failed measurements 
- * @param success_count number of successful measurements 
- * @param is_active true - last conversion was successful, false - otherwise
- * @param is_converting true - sensor is performing temperature conversion, false - conversion done or failed
- * @param conversion_started ticks timestamp for last conversion
- * @param scratchpad buffer for data received from DS18B20
+ * @brief ds18b20 sensor struct
+ * @param data_pin onewire data pin 
+ * @param temp_x10 last measured temperature in fixed point format e.g. 225 means 22.5°C. being updated only after valid conversion
+ * @param last_valid_conversion timestamp (ticks) when last valid conversion was received and temperature was updated 
+ * @param conversion_started timestamp (ticks) when last conversion was started
  */
 typedef struct
 {
     gpio_pin_t data_pin;
-    int16_t temperature_x10;
-    uint32_t error_count;
-    uint32_t success_count;
-    bool is_active;
-    bool is_converting;
+    int16_t temp_x10;
+    uint32_t last_valid_conversion;
     uint32_t conversion_started;
-    uint8_t scratchpad[9];
 } ds18b20_sensor_t;
 
 
 /**
- * @brief initialize ds18b20 sensor 
- * @param sensor ds18b20 sensor pointer
+ * @brief initialize ds18b20 sensor and start first conversion
+ * @note after init (no matter successful or not) and before first valid conversion .temp_x10 == DS18B20_INIT_VALUE (85.0`C) 
+ * @param sensor ds18b20 sensor struct pointer
  * @param data_pin onewire bus data pin 
+ * @return true sensor initialized (presence detected, first conversion started), false init failed (no presence)
  */
-void ds18b20_init(ds18b20_sensor_t *sensor, gpio_pin_t data_pin);
+bool ds18b20_init(ds18b20_sensor_t *sensor, gpio_pin_t data_pin);
 
 /**
  * @brief start non-blocking DS18B20 temperature conversion or update temperature if conversion done
- * @param sensor ds18b20 sensor pointer
+ * @note .temp_x10 being updated only after successful conversion, after failed one it remains unchanged
+ *       .temp_x10 == DS18B20_INIT_VALUE before at least one conversion was properly done.
+ *       DS18B20 sensor can't measure exactly 85.0`C. because there are no way to distinguish valid 85.0`C from the initial sensor value 
+ * @param sensor ds18b20 sensor struct pointer
+ * @param temp optional pointer if temperature in float needed or NULL. 
+ * @return last valid temperature "age" in microseconds. if there is no valid data yet return DS18B20_TEMP_NEVER_UPDATED
  */
-void ds18b20_update(ds18b20_sensor_t *sensor);
+uint32_t ds18b20_update(ds18b20_sensor_t *sensor, float *temp);
 
 #ifdef __cplusplus
 }
